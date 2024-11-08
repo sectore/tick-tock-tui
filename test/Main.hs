27edca459d9acb39e5d9946440b5084583a42648
jk <47693+sectore@users.noreply.github.com>
2024-11-08 18:54:40 +0100

    Improve `Show` instances
    to add a thousand separator. Don't trim `Fiat` decimals anymore. More tests.

--- test/Main.hs
+++ test/Main.hs
@@ -113,28 +113,35 @@ main = hspec $ do
         fiatToSats (Amount 50_000.09 :: Amount USD) (Price 200_000 :: Price USD) `shouldBe` (Amount 25000045 :: Amount SATS)
 
   describe "Show Amount a / ShowAmount" $ do
-    it "USD" $
-      show (Amount 50_000 :: Amount USD) `shouldBe` "USD 50000"
+    it "USD" $ do
+      show (Amount 5_000_000 :: Amount USD) `shouldBe` "USD 5,000,000.00"
+      show (Amount 5_000.99 :: Amount USD) `shouldBe` "USD 5,000.99"
+      show (Amount 5 :: Amount USD) `shouldBe` "USD 5.00"
     it "EUR" $ do
-      show (Amount 10_000 :: Amount EUR) `shouldBe` "EUR 10000"
+      show (Amount 10_000 :: Amount EUR) `shouldBe` "EUR 10,000.00"
       -- check some rounded cases
-      show (Amount 10_000.999 :: Amount EUR) `shouldBe` "EUR 10001"
-      show (Amount 10_000.99 :: Amount EUR) `shouldBe` "EUR 10000.99"
-      show (Amount 10_000.009 :: Amount EUR) `shouldBe` "EUR 10000.01"
-      show (Amount 10_000.001 :: Amount EUR) `shouldBe` "EUR 10000"
+      show (Amount 10_000.999 :: Amount EUR) `shouldBe` "EUR 10,001.00"
+      show (Amount 10_000.99 :: Amount EUR) `shouldBe` "EUR 10,000.99"
+      show (Amount 10_000.009 :: Amount EUR) `shouldBe` "EUR 10,000.01"
+      show (Amount 10_000.001 :: Amount EUR) `shouldBe` "EUR 10,000.00"
     it "AUD" $
-      show (Amount 75_000 :: Amount AUD) `shouldBe` "AUD 75000"
+      show (Amount 75_000 :: Amount AUD) `shouldBe` "AUD 75,000.00"
     it "CAD" $
-      show (Amount 25_000 :: Amount CAD) `shouldBe` "CAD 25000"
+      show (Amount 25_000 :: Amount CAD) `shouldBe` "CAD 25,000.00"
     it "JPY" $
-      show (Amount 100_000 :: Amount JPY) `shouldBe` "JPY 100000"
+      show (Amount 100_000 :: Amount JPY) `shouldBe` "JPY 100,000.00"
     it "CHF" $
-      show (Amount 15_000.01 :: Amount CHF) `shouldBe` "CHF 15000.01"
+      show (Amount 15_000.01 :: Amount CHF) `shouldBe` "CHF 15,000.01"
     it "GBP" $
-      show (Amount 30_000 :: Amount GBP) `shouldBe` "GBP 30000"
+      show (Amount 30_000 :: Amount GBP) `shouldBe` "GBP 30,000.00"
     it "BTC" $ do
-      show (Amount 1.23 :: Amount BTC) `shouldBe` "BTC 1.230 000 00"
-      show (Amount 1.00176761 :: Amount BTC) `shouldBe` "BTC 1.001 767 61"
+      show (Amount 1.23 :: Amount BTC) `shouldBe` "BTC 1.23 000 000"
+      show (Amount 1.00176761 :: Amount BTC) `shouldBe` "BTC 1.00 176 761"
+      show (Amount 10010.00000001 :: Amount BTC) `shouldBe` "BTC 10,010.00 000 001"
+      -- round down
+      show (Amount 0.0000000123 :: Amount BTC) `shouldBe` "BTC 0.00 000 001"
+      -- round up
+      show (Amount 0.000000019 :: Amount BTC) `shouldBe` "BTC 0.00 000 002"
     it "sats" $
       show (Amount 123 :: Amount SATS) `shouldBe` "123 sats"
 
@@ -208,3 +215,7 @@ main = hspec $ do
             read "1 sats" `shouldBe` (Amount 1 :: Amount SATS)
             evaluate (read "123456" :: Amount SATS)
             `shouldThrow` errorCall "Prelude.read: no parse"
+  describe "Misc" $ do
+    it "chunksOf" $ do
+      chunksOf 5 "tick-tock-next-block" `shouldBe` ["tick-", "tock-", "next-", "block"]
+      chunksOf 2 [0 .. 9 :: Int] `shouldBe` [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]]
