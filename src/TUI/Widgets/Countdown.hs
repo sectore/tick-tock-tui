0334faaceacdce32eaf5da6f95852baefa142c45
jk <47693+sectore@users.noreply.github.com>
2024-11-11 21:19:21 +0100

    remove label

--- src/TUI/Widgets/Countdown.hs
+++ src/TUI/Widgets/Countdown.hs
@@ -4,25 +4,21 @@ import Brick.Types
   ( Widget,
   )
 import Brick.Widgets.Core
-  ( hBox,
-    hLimit,
+  ( hLimit,
     padLeftRight,
     str,
+    (<+>),
   )
 import Brick.Widgets.ProgressBar qualified as P
 import Lens.Micro ((^.))
-import TUI.Types (TUIResource (..), TUIState, extraInfo, fetchTick, lastFetchTick)
+import TUI.Types (TUIResource (..), TUIState, fetchTick, lastFetchTick)
 import TUI.Utils (fps, maxFetchTick)
 import Text.Printf (printf)
 
 drawCountdown :: TUIState -> Widget TUIResource
 drawCountdown st =
-  hBox $
-    [ str "auto reload" | st ^. extraInfo
-    ]
-      <> [ progress,
-           tickTime
-         ]
+  progress
+    <+> tickTime
   where
     remainingTick = maxFetchTick - (st ^. fetchTick - st ^. lastFetchTick)
     percent :: Float
